78675d9c005ab04062bfe57f7c65a6b234228a22
zachjustice <zach.justice@slalom.com>
2017-10-23 19:56:51 -0400

    Added topic repository

---
+++ src/main/java/saturday/repositories/TopicRepository.java
@@ -0,0 +1,15 @@
+package saturday.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import saturday.domain.Entity;
+import saturday.domain.Topic;
+
+/**
+ * Created by zachjustice on 7/26/17.
+ */
+@Repository
+public interface TopicRepository extends JpaRepository<Topic, Long> {
+    Topic findByName(String name);
+    Topic findById(int id);
+}
