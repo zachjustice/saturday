52d3f1481bf246ff1f27a47d85109d294925a9e6
zachjustice <zach.justice@slalom.com>
2017-11-23 09:22:45 -0500

    Add topic invite repo

---
+++ src/main/java/saturday/repositories/TopicInviteRepository.java
@@ -0,0 +1,18 @@
+package saturday.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import saturday.domain.Entity;
+import saturday.domain.TopicContent;
+import saturday.domain.TopicInvite;
+
+import java.util.List;
+
+@Repository
+public interface TopicInviteRepository extends JpaRepository<TopicInvite, Long> {
+    List<TopicInvite> findByTopicId(int id);
+    List<TopicInvite> findTopicInvitesByInvitee(Entity invitee);
+    List<TopicInvite> findTopicInvitesByInviter(Entity inviter);
+    TopicInvite findById(int id);
+    TopicInvite delete(int id);
+}
