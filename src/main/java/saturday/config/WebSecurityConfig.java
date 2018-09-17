d79be150aace0df914ff060b5f2c2c8da420a508
zachjustice <zachjustice123@gmail.com>
2018-09-17 18:48:01 -0400

    Make share/{invite_link} publicly exposed

--- src/main/java/saturday/config/WebSecurityConfig.java
+++ src/main/java/saturday/config/WebSecurityConfig.java
@@ -63,6 +63,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.PUT, "/confirm_email").permitAll()
                 //.antMatchers(HttpMethod.GET, "/send_confirmation_email").permitAll()
 
+                .antMatchers(HttpMethod.OPTIONS, "/share/*").permitAll()
+                .antMatchers(HttpMethod.GET, "/share/*").permitAll()
+
                 .antMatchers(HttpMethod.OPTIONS, "/validate_access_token").permitAll()
                 .antMatchers(HttpMethod.POST, "/validate_access_token").permitAll()
 
